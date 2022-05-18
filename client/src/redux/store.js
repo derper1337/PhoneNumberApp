b4b0029d87aae2f04e905cb0b0e4506acb56aa6e
Philipp Dramoretskij <dramoretskij.fv@gmail.com>
2022-05-18 06:51:17 +0300

    remove features that i used for debugging
    remove features that i used for debugging

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
 import appReducer from "./appReducer";
-import thunkMiddleware from "redux-thunk";
 
 export const store = configureStore({
     reducer:{
         app:appReducer,
     }
 });
-window.store = store;
